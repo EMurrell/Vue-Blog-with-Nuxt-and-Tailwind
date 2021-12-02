9babf0826246d91cd78f8a1b59328f391a511fbe
EMurrell <82235988+EMurrell@users.noreply.github.com>
2021-12-02 10:17:13 -0500

    fix navigation

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,18 +1,18 @@
 <template>
-  <article class="grid grid-cols-12 pb-24 pt-6">
+  <article class="grid grid-cols-12 pt-6 pb-24">
     <nuxt-img
-      class="rounded-lg col-start-3 col-span-8 w-full"
+      class="w-full col-span-8 col-start-3 rounded-lg"
       :src="post.image"
       width="768"
       height="409"
     />
 
-    <div class="w-full col-start-1 col-span-12 grid grid-cols-12">
-      <nav class="mt-8 col-span-2 shadow-2xl rounded-lg p-4 self-start">
+    <div class="grid w-full grid-cols-12 col-span-12 col-start-1">
+      <nav class="self-start col-span-2 p-4 mt-8 rounded-lg shadow-2xl">
         <ul class="space-y-2">
           <li v-for="link of post.toc" :key="link.id">
             <NuxtLink
-              class="uppercase tracking-wider text-gray-500 hover:underline"
+              class="tracking-wider text-gray-500 uppercase hover:underline"
               :to="`#${link.id}`"
               >{{ link.text }}</NuxtLink
             >
@@ -20,21 +20,18 @@
           </li>
         </ul>
       </nav>
-      <div class="col-start-4 col-span-6 w-full">
+      <div class="w-full col-span-6 col-start-4">
         <div>
-          <NuxtLink to="/" href="/" class="block text-gray-400 mt-6"
-            >← Go Back</NuxtLink
-          >
-          <ul v-if="post.tags" class="flex space-x-3 mt-2">
+          <ul v-if="post.tags" class="flex mt-2 space-x-3">
             <li
               v-for="tag in post.tags"
               :key="tag"
-              class="text-gray-400 font-bold"
+              class="font-bold text-gray-400"
             >
               {{ tag }}
             </li>
           </ul>
-          <h1 class="text-5xl font-black mt-2">{{ post.title }}</h1>
+          <h1 class="mt-2 text-5xl font-black">{{ post.title }}</h1>
         </div>
         <nuxt-content class="mt-4 prose max-w-none" :document="post" />
       </div>
